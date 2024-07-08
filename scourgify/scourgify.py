422697c0ae031386e75aa79df6b02b14ade8f5d6
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-08 11:08:44 +0100

    Scourgify function: write new rows in newfile

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,7 +2,7 @@ import csv, sys
 
 def main():
     # If not 3 arguments, exit
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         sys.exit("Too few or many arguments.")
     # Else scourgify
     else:
@@ -12,15 +12,19 @@ def scourgify():
     # Open file and read "name" and "house"
     list = []
     with open(sys.argv[1], "r") as file:
-        reader = csv.reader(file)
+        reader = csv.DictReader(file)
         try:
             for name, house in reader:
-                list.append({"name": name, "house": house})
-            return name, house
+                first, last = name.split(",")
+                list.append({"first": first, "last": last, "house": house})
+                # Split "name" into "first" and "last" and write that and "house" into argv[3]
+                with open(sys.argv[3], "a") as newfile:
+                    writer = csv.DictWriter(newfile, fieldnames["first", "last", "house"])
+                    writer.writerow({"first": first, "last": last, "house": house})
+        # If not readable, exit
         except FileNotFoundError:
             sys.exit("File not readable")
-    # If not readable, exit
-    # Split "name" into "first" and "last" and write that and "house" into argv[3]
+
 
 if __name__ == "__main__":
     main()
