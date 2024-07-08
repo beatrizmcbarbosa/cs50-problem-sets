aab17da2d8dd019f3afbbcea20ede02d72ecd60e
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-08 10:42:13 +0100

    First commit

---
+++ scourgify/scourgify.py
@@ -0,0 +1,11 @@
+def main():
+    # If not 3 arguments, exit
+    # If not readable, exit
+    # Else scourgify
+
+def scourgify():
+    # Open file and read "name" and "house"
+    # Split "name" into "first" and "last" and write that and "house" into argv[3]
+
+if __name__ == "__main__":
+    main()
