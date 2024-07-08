0cd811e73a50180c1688082af9bbcf2658894c9e
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-08 11:02:14 +0100

    Scourgify function: read through csv file and append to list

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,10 +1,25 @@
+import csv, sys
+
 def main():
     # If not 3 arguments, exit
-    # If not readable, exit
+    if len(sys.argv) != 2:
+        sys.exit("Too few or many arguments.")
     # Else scourgify
+    else:
+        print(scourgify())
 
 def scourgify():
     # Open file and read "name" and "house"
+    list = []
+    with open(sys.argv[1], "r") as file:
+        reader = csv.reader(file)
+        try:
+            for name, house in reader:
+                list.append({"name": name, "house": house})
+            return name, house
+        except FileNotFoundError:
+            sys.exit("File not readable")
+    # If not readable, exit
     # Split "name" into "first" and "last" and write that and "house" into argv[3]
 
 if __name__ == "__main__":
