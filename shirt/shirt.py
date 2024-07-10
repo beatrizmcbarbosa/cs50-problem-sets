3d04907e91bff1de0edd879990915260c4615c5e
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-10 13:40:50 +0100

    Edit image_overlay function

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys
 
 from PIL import Image, ImageOps
 
@@ -8,32 +8,35 @@ def main():
     if len(sys.argv) != 3:
         sys.exit("Too few or many arguments")
     # Exit if the input’s and output’s names do not end in .jpg, .jpeg, or .png, case-insensitively
-    elif sys.argv[1].endswith([".jpg", ".jpeg", ".png"]) == False or sys.argv[2].endswith([".jpg", ".jpeg", ".png"]) == False:
-        sys.exit("Wrong format")
     # Exit if the input’s name does not have the same extension as the output’s name
-    elif os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
-        sys.exit("Different formats")
+    elif (sys.argv[1]).split(".")[1] == (sys.argv[2]).split(".")[1]:
+        # Exit if the input’s and output’s names do not end in .jpg, .jpeg, or .png, case-insensitively
+        if(
+            (sys.argv[1]).endswith(".jpg")
+            or (sys.argv[1]).endswith(".jpeg")
+            or (sys.argv[1]).endswith(".png")
+            ):
+            image_overlay()
+        else:
+            sys.exit("Invalid format")
     else:
-        image_overlay()
+        sys.exit("Different extensions")
 
 def image_overlay():
     try:
         # Open input: image
-        inputimage, shirt = Image.open(sys.argv[1], sys.argv[2])
-        size = shirt.size
-        # Resize input: image
-        cropped = ImageOps.fit(inputimage, size)
-        # Overlay tshirt on input image
-        overlay = cropped.paste(inputimage, shirt)
-        print(type(overlay))         # This type(...) has helped me out
-                                        # of a jam many times !!!
-        # Produce output: tshirt on image in a new file
-        overlay.save(sys.argv[2])
-
+        inputimage = Image.open(sys.argv[1])
     # Exit if the specified input does not exist.
     except FileNotFoundError:
-        sys.exit("File not readable")
+        sys.exit("File not found")
+    shirt = Image.open("shirt.png")
+    size = shirt.size
+    # Resize input: image
+    cropped = ImageOps.fit(inputimage, size)
+    # Overlay tshirt on input image
+    cropped.paste(shirt, shirt)
+    # Produce output: tshirt on image in a new file
+    cropped.save(sys.argv[2])
 
 if __name__ == "__main__":
     main()
-
