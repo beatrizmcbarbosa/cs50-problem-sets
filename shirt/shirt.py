3cca3d78d29563ccba937a1469a28d89d8a958c1
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-10 11:22:22 +0100

    First commit: plan

---
+++ shirt/shirt.py
@@ -0,0 +1,15 @@
+def main():
+
+# Command line arguments check
+# Exit if the user does not specify exactly two command-line arguments,
+# Exit if the input’s and output’s names do not end in .jpg, .jpeg, or .png, case-insensitively,
+# Exit if the input’s name does not have the same extension as the output’s name, or
+# Exit if the specified input does not exist.
+
+# Open input: image
+# Resize input: image
+# Overlay tshirt on input image
+# Produce output: tshirt on image in a new file
+if __name__ == "__main__":
+    main()
+
