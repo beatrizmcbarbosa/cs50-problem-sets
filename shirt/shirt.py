50c6fc450252bcdba2b9e57abfd101cde844bc3e
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-10 12:14:18 +0100

    Image overlay function

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,15 +1,39 @@
+import sys, os
+
+from PIL import Image, ImageOps
+
 def main():
+    # Command line arguments check
+    # Exit if the user does not specify exactly two command-line arguments
+    if len(sys.argv) != 3:
+        sys.exit("Too few or many arguments")
+    # Exit if the input’s and output’s names do not end in .jpg, .jpeg, or .png, case-insensitively
+    elif sys.argv[1].endswith([".jpg", ".jpeg", ".png"]) == False or sys.argv[2].endswith([".jpg", ".jpeg", ".png"]) == False:
+        sys.exit("Wrong format")
+    # Exit if the input’s name does not have the same extension as the output’s name
+    elif os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
+        sys.exit("Different formats")
+    else:
+        image_overlay()
 
-# Command line arguments check
-# Exit if the user does not specify exactly two command-line arguments,
-# Exit if the input’s and output’s names do not end in .jpg, .jpeg, or .png, case-insensitively,
-# Exit if the input’s name does not have the same extension as the output’s name, or
-# Exit if the specified input does not exist.
+def image_overlay():
+    try:
+        # Open input: image
+        inputimage, shirt = Image.open(sys.argv[1], sys.argv[2])
+        size = shirt.size
+        # Resize input: image
+        cropped = ImageOps.fit(inputimage, size)
+        # Overlay tshirt on input image
+        overlay = cropped.paste(inputimage, shirt)
+        print(type(overlay))         # This type(...) has helped me out
+                                        # of a jam many times !!!
+        # Produce output: tshirt on image in a new file
+        overlay.save(sys.argv[2])
 
-# Open input: image
-# Resize input: image
-# Overlay tshirt on input image
-# Produce output: tshirt on image in a new file
+    # Exit if the specified input does not exist.
+    except FileNotFoundError:
+        sys.exit("File not readable")
+
 if __name__ == "__main__":
     main()
 
