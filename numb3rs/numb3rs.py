0bbe795e89c7198a5fdad4af09d73bdef480a763
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-21 17:55:31 +0100

    Fix regular expression

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,23 +1,23 @@
 import re
-import sys
 
 
 def main():
-    print(validate(input("IPv4 Address: ")))
+    print(validate(input("IPv4 Address: ").strip()))
 
 
 def validate(ip):
     # Typical format #.#.#.#, each # is a number between 0 and 255 (not included)
-    pattern = r"^[1-255].[1-255].[1-255].[1-255]"
-    match = re.search(pattern, ip, re.IGNORECASE)
+    pattern = r"^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$"
+    match = re.search(pattern, ip)
 
     if match:
+        for i in range(1, 5):
+            if int(match.group(i)) > 255:
+                return False
         return True
     else:
         return False
 
-
-...
 
 
 if __name__ == "__main__":
