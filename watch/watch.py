2968583a96f3082f52ed957b39f820134a3f8eb5
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-07-22 13:23:01 +0100

    Edited input requirements (regular expression

--- watch/watch.py
+++ watch/watch.py
@@ -6,9 +6,10 @@ def main():
 
 def parse(s):
     if matches := re.search(
-        # https://www.youtube.com/embed/xvFZjo5PgG0
-        r"^https?:\/\/(?:www\.)?youtube\.com\/embed\/([a-z0-9_]+)", s, re.IGNORECASE
+        # <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
+        r'^<iframe src="https?:\/\/(?:www\.)?youtube\.com\/embed\/([a-z0-9_]+)"><\/iframe>', s, re.IGNORECASE
         ):
+        # https://youtu.be/xvFZjo5PgG0
         return (f"https://youtu.be/{matches.group(1)}")
     else:
         return None
