672174bbcd3b29e26c52b26bd71ab82bee3b445d
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-03 17:57:34 +0100

    Create class

--- packages/packages.py
+++ packages/packages.py
@@ -1,9 +1,15 @@
 class Package:
-    ...
+    def __init__(self, number, sender, recipient, weight):
+        self.number = number
+        self.sender = sender
+        self.recipient = recipient
+        self.weight = weight
     
     
 def main():
-    packages = ["Package 1: Alice to Bob, 10kg", "Package 2: Bob to Charlie, 5kg"]
-    
+    packages = [
+        Package(number=1, sender="Alice", recipient="Bob", weight=10),
+        Package(number=2, sender="Bob", recipient="Charlie", weight=5)
+    ]
 if __name__ == "__main__":
     main()
