67d63af679322283f653c22693a2767b5e01ae72
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-03 18:49:41 +0100

    get method

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,11 +1,26 @@
 from datetime import date
+import inflect
 
+class Seasons:
+    def __init__(self, date, minutes):
+        self.date = date
+        self.minutes = minutes
+
+    def __str__(self):
+        ...
+
+    def calculate_minutes(self):
+        ...
+
+    @classmethod
+    def get(cls):
+        date = input("Date of Birth: ")
+        return cls(date)
 
 def main():
     ...
 
 
-...
 
 
 if __name__ == "__main__":
