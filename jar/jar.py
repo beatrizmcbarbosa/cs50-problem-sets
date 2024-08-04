513e4f3ed6b508ccf8d2a2b59e644ff6920be806
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-04 14:48:17 +0100

    Define methods and rules

--- jar/jar.py
+++ jar/jar.py
@@ -1,20 +1,26 @@
 class Jar:
+    # Initialise cookir jar with capacity
+    # If capacity is not a non-negative int, raise ValueError
     def __init__(self, capacity=12):
         ...
-
+    # Return a string with 🍪 * number of cookies in the jar. 3 cookies == 🍪🍪🍪
     def __str__(self):
         ...
 
+    # Add n cookies to the jar. If adding more than capacity, raise ValueError
     def deposit(self, n):
         ...
 
+    # Remove n cookies from the jar. If n is higher than the cookies existing in the jar, raise ValueError
     def withdraw(self, n):
         ...
 
+    # Return jar's capacity
     @property
     def capacity(self):
         ...
 
+    # Return number of cookies in the jar, initially 0
     @property
     def size(self):
         ...
