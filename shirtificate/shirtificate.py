3db06843b106ce287d4df834ae29d3b6ed3713dc
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-04 17:57:19 +0100

    Updated convert function

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,21 +5,25 @@ def main():
     user = input("Name: ")
     print(convert(user))
 
-
 def convert(name):
     # Create page, format and orientation
-    pdf = FPDF(orientation="portrait", format="A4")
-    pdf.add_page()
+    pdf = FPDF()
+    pdf.add_page(orientation="portrait", format="A4")
+
     # Add image
-    pdf.image("shirtificate/shirtificate.png", x=20, y=60)
-    # Set text font and font size
-    pdf.set_font("helvetica", size=12)
-    # Add text in the middle
-    pdf.cell(text=f"{name} took CS50")
+    pdf.image("shirtificate/shirtificate.png", x=10, y=70, h=200, w=190)
+
     # Add text on top
-    pdf.cell(text="CS50 Shirtificate")
+    pdf.set_font("helvetica", size=40)
+    pdf.cell(0, 30, f"CS50 Shirtificate", align="C")
+
+    # Add text in the middle
+    pdf.set_font("helvetica", size=25)
+    pdf.set_text_color(255, 255, 255)
+    pdf.cell(-180, 220, f"{name} took CS50", align="C")
+
     # Output pdf
-    pdf.output("shirtificate.pdf")
+    pdf.output("shirtificate/shirtificate.pdf")
 
 
 if __name__ == "__main__":
