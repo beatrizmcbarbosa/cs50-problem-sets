c1376e93d73ed3a4dc7f223c06cfc3b984c056b6
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-04 10:28:15 +0100

    Remove classes and use built-in date class

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,26 +1,31 @@
 from datetime import date
+import sys
 import inflect
 
-class Seasons:
-    def __init__(self, date, minutes):
-        self.date = date
-        self.minutes = minutes
-
-    def __str__(self):
-        ...
-
-    def calculate_minutes(self):
-        ...
-
-    @classmethod
-    def get(cls):
-        date = input("Date of Birth: ")
-        return cls(date)
+p = inflect.engine()
 
 def main():
+    # Get brithday date
+    dob = input("Date of Birth: ")
+    # Print birthday as minutes, using inflect method
+    print(days_to_minutes(dob))
+
+def det_dob(s):
+    # If birthday date is valid, convert to iso
+    try:
+        iso_date = date.fromisoformat(s)
+    # If date is not valid, exit
+    except:
+        sys.exit("Invalid date")
+    # Return days
+    return (date.today - iso_date).day
+
+
+def days_to_minutes():
+    # Subtract birthday from today's date, using timedelta. Today's date is given by date class
     ...
-
-
+    # Convert days to minutes
+    ...
 
 
 if __name__ == "__main__":
